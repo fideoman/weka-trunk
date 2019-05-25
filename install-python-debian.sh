41f708cb207ddaf860e8c0d58fa3642e2228e72f
fideoman <idsl1012@gmail.com>
2019-05-24 23:09:30 -0400

    Isaac: python3-tk missing on script.

--- install-python-debian.sh
+++ install-python-debian.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install python3 python3-scipy python3-sklearn python3-matplotlib python3-numpy python3-pandas
+sudo apt-get install python3 python3-scipy python3-sklearn python3-matplotlib python3-numpy python3-pandas python3-tk
 
 update-alternatives --install /usr/bin/python python /usr/bin/python3 1
 
