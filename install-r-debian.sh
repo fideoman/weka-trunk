86c4a2c5c76fe34445bf9af617b04c9aae95c3c4
fideoman <idsl1012@gmail.com>
2019-05-27 08:22:25 -0300

    Isaac: Adding extra package for R.

--- install-r-debian.sh
+++ install-r-debian.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install r-cran-rjava r-cran-xml openjdk-11-jdk-headless libxml2-dev
+sudo apt-get install r-cran-rjava r-cran-xml r-cran-mlr openjdk-11-jdk-headless libxml2-dev
 
 echo "After this script, make following env vars visible on your machine:"
 echo "export R_HOME=\"/usr/lib/R\"; export R_LIBS_USER=\"/usr/lib/R/site-library\";"
