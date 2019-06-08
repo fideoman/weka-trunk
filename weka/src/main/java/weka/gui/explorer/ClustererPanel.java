0bae09a65fcf6cd0fc9150fb21c8f94f06b27aa9
eibe <eibe@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2019-06-08 09:00:43 +0000

    Had to undo last change because it broke loading and saving int the ClustererPanel.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk@15201 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- weka/src/main/java/weka/gui/explorer/ClustererPanel.java
+++ weka/src/main/java/weka/gui/explorer/ClustererPanel.java
@@ -821,7 +821,6 @@ public class ClustererPanel extends AbstractPerspective implements
                 + Utils.joinOptions(((OptionHandler) m_ClustererEditor
                   .getValue()).getOptions());
           }
-          Instances trainInst = null; // We need this if we want to serialise the model
           try {
             m_Log.logMessage("Started " + cname);
             m_Log.logMessage("Command: " + cmd);
@@ -852,7 +851,7 @@ public class ClustererPanel extends AbstractPerspective implements
               throw new Exception("Unknown test mode");
             }
 
-            trainInst = new Instances(inst);
+            Instances trainInst = new Instances(inst);
             if (m_ClassesToClustersBut.isSelected()) {
               trainInst.setClassIndex(m_ClassCombo.getSelectedIndex());
               inst.setClassIndex(m_ClassCombo.getSelectedIndex());
@@ -1064,10 +1063,8 @@ public class ClustererPanel extends AbstractPerspective implements
 
               ArrayList<Object> vv = new ArrayList<Object>();
               vv.add(fullClusterer);
-              if (trainInst != null) {
-                Instances trainHeader = new Instances(trainInst, 0);
-                vv.add(trainHeader);
-              }
+              Instances trainHeader = new Instances(m_Instances, 0);
+              vv.add(trainHeader);
               if (ignoredAtts != null) {
                 vv.add(ignoredAtts);
               }
